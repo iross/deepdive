aa09304f8246d53bb07095ef035aa51475e29d2e
Ian Ross <iross@cs.wisc.edu>
2015-01-26 16:00:57 -0600

    Uncomment all that stuff

--- createTessNLPJobs.sh
+++ createTessNLPJobs.sh
@@ -12,19 +12,19 @@ journal_clean=${journal_clean// /_}
 journal_out="$journal_clean"_out
 journal_new="$journal_clean"_NLP
 
-#for job in "$journal_out"/job*/;
-#do
-#    if ls $job/*hocr.html 1> /dev/null 2>&1; then
-#        mkdir -p $journal_new${job/$journal_out/}
-#        ln -s `pwd`/$job/*hocr.html $journal_new${job/$journal_out/}
-#    fi
-#done
+for job in "$journal_out"/job*/;
+do
+    if ls $job/*hocr.html 1> /dev/null 2>&1; then
+        mkdir -p $journal_new${job/$journal_out/}
+        ln -s `pwd`/$job/*hocr.html $journal_new${job/$journal_out/}
+    fi
+done
 
 #carry the filepath mapping pickle along for easier bookkeeping
 cp $journal_clean/filepath_mapping.pickle $journal_new/
 
 # assume ChtcRun/NLPshared exists and holds all common shared junk
-#cp -r NLPshared $journal_new
-#mv $journal_new/NLPshared $journal_new/shared
+cp -r NLPshared $journal_new
+mv $journal_new/NLPshared $journal_new/shared
 echo "Submit directories created from requested output! Use mkdag to create DAG files for submission. e.g.:"
 echo ./mkdag --cmdtorun=do.sh --data=$journal_new --outputdir="$journal_new"_out_NLP --pattern=SUCCEED.txt --type=other                                                                                 
