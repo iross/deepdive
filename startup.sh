976c50f29338ffe6ee8a89d42a360488796872c6
Ian Ross <iross@cs.wisc.edu>
2015-02-19 10:50:05 -0600

    Add Merlin accounting group to the process template

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 git checkout ians_cde
-wget http://chtc.cs.wisc.edu/downloads/ChtcRun.tar.gz
-tar xzf ChtcRun.tar.gz
+wget http://pages.cs.wisc.edu/~iross/ChtcRun_Merlin.tar.gz
+tar xzf ChtcRun_Merlin.tar.gz
 cd ChtcRun
 mkdir shared
 cp ../URLS shared/
