50cdfeef8104bf56770c63222b9e60ed2174131d
Ian Ross <iross@cs.wisc.edu>
2015-02-19 09:24:44 -0600

    Tiny updates

--- createJobs.sh
+++ createJobs.sh
@@ -5,8 +5,9 @@ journal=$1
 journal_clean=${journal//,/}
 journal_clean=${journal_clean// /_}
 
-python ../deepdive/pdf_to_dag.py ~/DeepDive/downloads/$journal $journal_clean
+echo "Running pdf_to_dag with args $2 and submit_$journal_clean"
+python ../deepdive/pdf_to_dag.py "$2" "submit_$journal_clean"
 
 # assume ChtcRun/shared exists and holds all common shared junk
-cp -r shared/ $journal_clean/
-./mkdag --cmdtorun=ocr_pdf.py --parg=input.pdf --parg="--cuneiform" --parg="--tesseract" --data=$journal_clean --outputdir="$journal_clean"_out --pattern=*.html --type=other                                                                                 
+cp -rL shared/ submit_$journal_clean/
+echo ./mkdag --cmdtorun=ocr_pdf.py --parg=input.*f --parg="--cuneiform" --parg="--tesseract" --parg="--pdf" --data=submit_$journal_clean --outputdir="$journal_clean"_out --pattern=*.html --type=other
