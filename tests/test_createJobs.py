93ae6fa2b84027508ebe5ad85e6f9edb82f45f65
Ian Ross <iross@cs.wisc.edu>
2015-02-24 14:40:20 -0600

    Add dummy tests for createJobs

---
+++ tests/test_createJobs.py
@@ -0,0 +1,21 @@
+from createJobs import *
+import unittest
+
+class TestCreateJobs(unittest.TestCase):
+    """Test the downloadManager class"""
+
+    setUpRun = False
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_remote_submit(self):
+        # make sure the dir is created as expected
+        # make sure that the mkdag command is what we want.
+        self.assertTrue(True)
+
+    def test_local_submit(self):
+        self.assertTrue(True)
