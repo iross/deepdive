ef8712340b723813ee411f472249d5de2affb1fa
Ian Ross <iross@cs.wisc.edu>
2015-03-13 10:05:29 -0500

    Catch errors from pdf_to_dag + don't try to continue

--- createJobs.sh
+++ createJobs.sh
@@ -10,9 +10,14 @@ joblimit=$3
 echo "Running pdf_to_dag with args $2 and submit_$journal_clean"
 python ../pdf_to_dag.py "$2" "submit_$journal_clean" $3
 
-# assume ChtcRun/shared exists and holds all common shared junk
-for f in submit_$journal_clean*/; do
-    cp -rL shared/ $f
-    f=${f/\//}
-    echo ./mkdag --cmdtorun=ocr_pdf.py --parg=input.*f --parg="--cuneiform" --parg="--tesseract" --parg="--pdf" --data=$f --outputdir="${f/submit_/}"_out --pattern=*.html --type=other
-done
+if [ $? -eq 0 ]; then
+    # assume ChtcRun/shared exists and holds all common shared junk
+    for f in submit_$journal_clean*/; do
+        cp -rL shared/ $f
+        f=${f/\//}
+        echo ./mkdag --cmdtorun=ocr_pdf.py --parg=input.*f --parg="--cuneiform" --parg="--tesseract" --parg="--pdf" --data=$f --outputdir="${f/submit_/}"_out --pattern=*.html --type=other
+    done
+else
+    echo "Error in creating input directory! Please make sure that the specified path ($2) exists!"
+fi
+
