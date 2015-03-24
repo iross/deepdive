a7cb327f024440b0d24b7a5b8983ef80f91b0f2c
Ian Ross <iross@cs.wisc.edu>
2015-03-24 11:03:46 -0500

    Add PDF recombiner

--- organize_htcondor_jobs.py
+++ organize_htcondor_jobs.py
@@ -11,6 +11,10 @@ __copyright__ = "Zhaoyu Luo"
 import os
 import pickle
 import shutil
+import subprocess
+import string
+import pdb
+import re
 
 
 def organize_htcondor_jobs(origin_input_folder,
@@ -39,25 +43,43 @@ def organize_htcondor_jobs(origin_input_folder,
 
     for job_name in os.listdir(htcondor_output_folder):
         job_dir_path = os.path.join(htcondor_output_folder, job_name)
+        job_dir_pdfs = []
         if job_name in input_info:
             # base on the original input folder path, cut the prefix
             new_organized_path = os.path.join(output_folder,
                 os.path.abspath(input_info[job_name])\
                     [len(origin_input_folder)+1:])
-            if os.path.exists(new_organized_path):
-                print "Warning: %s existed, skip copy from %s to %s"\
-                    % (new_organized_path, job_dir_path, new_organized_path)
-                continue
-            else:
+            new_organized_path = os.path.join(output_folder,
+                os.path.abspath(input_info[job_name])\
+                        [len(origin_input_folder)+1:input_info[job_name].rfind('/')])+"/"
+            original_file_name = os.path.abspath(input_info[job_name])\
+                    [input_info[job_name].rfind('/')+1:]
+            new_base = new_organized_path + original_file_name+"_"
+            if not os.path.exists(new_organized_path):
                 os.makedirs(new_organized_path)
 
             for f in os.listdir(job_dir_path):
                 if f != "out.txt" and \
                     (f.endswith('.txt') or f.endswith('.html') \
                     or f.endswith('.pdf') or f.endswith('.hocr') ):
+                    f_new = re.sub('(_input.+?f)', '', f)
                     shutil.copy(
                         os.path.join(htcondor_output_folder, job_name, f),
-                        new_organized_path)
+                        new_base + f_new)
+                if f.endswith('.pdf'):
+                    # separate cune/tess pdfs?
+                    job_dir_pdfs.append('./'+job_dir_path+'/'+f)
+
+            # smoosh multi-page PDFs into one PDF via gs:
+            # gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=merged.pdf mine1.pdf mine2.pdf
+            for type in ["tesseract", "cuneiform"]:
+                combined_output = "\"" + new_base + "%s_combined.pdf" % type + "\""
+                pdfstr = "%s/%s*.pdf" % (job_dir_path, type)
+                try:
+                    subprocess.call('./cde-package/cde-exec pdftk %s cat output %s' % (pdfstr, combined_output), shell=True )
+                except:
+                    pass
+
         else:
             if os.path.isdir(job_dir_path):
                 raise Exception("Please check %s,"
